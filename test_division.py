fe9921d7dffa60fb010dacf4d9822152f0b795df
DanteOnline <iamdanteonline@gmail.com>
2023-09-07 21:18:39 +0400

    add test

---
+++ test_division.py
@@ -0,0 +1,5 @@
+from calculator.division import division
+
+
+def test_division():
+    assert division(4, 2) == 2
---
+++ test_minimum.py
@@ -0,0 +1,5 @@
+from calculator.minimum import minimum
+
+
+def test_minimum():
+    assert minimum(4, 5) == 4
